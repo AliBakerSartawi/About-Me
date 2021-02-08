fdf0d2d0362b94ba4d0c8d45198a490660de72e9
AliBakerSartawi <alisartawi@live.com>
2021-02-08 16:25:13 +0200

    commented out consolelogs

--- js/app.js
+++ js/app.js
@@ -2,46 +2,46 @@
 
 let gender = prompt('If my name is Ali, am I a male?');
 if((gender.toLowerCase() === 'yes') || (gender.toLowerCase() === 'y')){
-  console.log('correct answer, I am male!');
+  // console.log('correct answer, I am male!');
   alert('Correct answer, I am male!');
 } else {
-  console.log('Too bad... wrong answer!');
+  // console.log('Too bad... wrong answer!');
   alert('Too bad... wrong answer!');
 }
 
 let coding = prompt('Do I love coding?');
 if((coding.toLowerCase() === 'yes') || (coding.toLowerCase() === 'y')){
-  console.log('correct answer!');
+  // console.log('correct answer!');
   alert('Correct!');
 } else {
-  console.log('wrong answer... sadge!!');
+  // console.log('wrong answer... sadge!!');
   alert('wrong answer... sadge!!');
 }
 
 let age = prompt('Am I 30 years old?');
 if((age.toLowerCase() === 'no') || (age.toLowerCase() === 'n')){
-  console.log('Good guess, I am 27!');
+  // console.log('Good guess, I am 27!');
   alert('Good guess, I am 27!');
 } else {
-  console.log('wrong answer :( !');
+  // console.log('wrong answer :( !');
   alert('wrong answer :( !');
 }
 
 let water = prompt('Is water my favorite juice?');
 if((water.toLowerCase() === 'no') || (water.toLowerCase() === 'n')){
-  console.log('Good guess, water is not a juice!');
+  // console.log('Good guess, water is not a juice!');
   alert('Good guess, water is not a juice!');
 } else {
-  console.log('wrong answer :( !');
+  // console.log('wrong answer :( !');
   alert('wrong answer :( !');
 }
 
 let peanutButter = prompt('Do I like peanut butter?');
 if((peanutButter.toLowerCase() === 'yes') || (peanutButter.toLowerCase() === 'y')){
-  console.log('Of course!!!! PB is the best!!');
+  // console.log('Of course!!!! PB is the best!!');
   alert('Of course!!!! PB is the best!!');
 } else {
-  console.log('wrong answer :( !');
+  // console.log('wrong answer :( !');
   alert('wrong answer :( !');
 }
 
