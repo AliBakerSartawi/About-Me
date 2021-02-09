a9de39d0aa067f5aadd30b174125686a0fe5341d
AliBakerSartawi <alisartawi@live.com>
2021-02-09 16:07:25 +0200

    q 6 and 7

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 ## About-Me
 
-This is an interactive website shaped as a quiz about a few questions about me
+This is an interactive website shaped as a quiz about a few questions about me.
+
+There are 7 questions... five of them are 'yes' and 'no' questions... the last two are guessing questions that require a loop function.
+
+The website includes some information about me, as well as bullet points of my previous education and experience, and a top-ten list of my favorite foods.
 
 Author: Ali Baker Sartawi
--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 'use strict';
 
+let corAnswer=0;
+
 let gender = prompt('If my name is Ali, am I a male?');
 if((gender.toLowerCase() === 'yes') || (gender.toLowerCase() === 'y')){
   // console.log('correct answer, I am male!');
   alert('Correct answer, I am male!');
+  corAnswer++;
 } else {
   // console.log('Too bad... wrong answer!');
   alert('Too bad... wrong answer!');
@@ -16,6 +19,7 @@ let coding = prompt('Do I love coding?');
 if((coding.toLowerCase() === 'yes') || (coding.toLowerCase() === 'y')){
   // console.log('correct answer!');
   alert('Correct!');
+  corAnswer++;
 } else {
   // console.log('wrong answer... sadge!!');
   alert('wrong answer... sadge!!');
@@ -25,6 +29,7 @@ let age = prompt('Am I 30 years old?');
 if((age.toLowerCase() === 'no') || (age.toLowerCase() === 'n')){
   // console.log('Good guess, I am 27!');
   alert('Good guess, I am 27!');
+  corAnswer++;
 } else {
   // console.log('wrong answer :( !');
   alert('wrong answer :( !');
@@ -34,6 +39,7 @@ let water = prompt('Is water my favorite juice?');
 if((water.toLowerCase() === 'no') || (water.toLowerCase() === 'n')){
   // console.log('Good guess, water is not a juice!');
   alert('Good guess, water is not a juice!');
+  corAnswer++;
 } else {
   // console.log('wrong answer :( !');
   alert('wrong answer :( !');
@@ -43,19 +49,58 @@ let peanutButter = prompt('Do I like peanut butter?');
 if((peanutButter.toLowerCase() === 'yes') || (peanutButter.toLowerCase() === 'y')){
   // console.log('Of course!!!! PB is the best!!');
   alert('Of course!!!! PB is the best!!');
+  corAnswer++;
 } else {
   // console.log('wrong answer :( !');
   alert('wrong answer :( !');
 }
 
 let userName = prompt('What is your name, dear visitor?');
-alert('Thank you for visiting my humble website, ' + userName + ' :)');
+alert('Thank you for visiting my humble website, ' + userName + ', but we are not done yet :)');
 
-// switch (key) {
-  // case value:
-  // case anotherValue: //this way we can have two values without repetition 
-    // break;
 
-  // default:
-    // break;
-// }
+
+
+let attempts=4;
+while(attempts){
+  let guess=prompt('Guess a number from 1 to 10! You have ' + attempts + ' attempts!');
+  if (guess==='5'){
+    alert('Correct answer... it is 5!');
+    corAnswer++;
+    break;
+  } else if(guess < '5'){
+    attempts-=1;
+    alert('Too low... you have ' + attempts + ' attempts left!');
+    if(!attempts){
+      alert('No more attempts.. correct answer is 5!');
+      break;
+    }
+  } else if(guess > '5'){
+    attempts-=1;
+    alert('Too high... you have ' + attempts + ' attempts left!');
+    if(!attempts){
+      alert('Correct answer is 5!');
+      break;
+    }
+  }
+}
+
+let ali=['a', 'l', 'i'];
+let newAttempts=6;
+while(newAttempts){
+  let newGuess = prompt('Guess a letter from my name! you have ' + newAttempts + ' attempts!').toLowerCase();
+  if ((newGuess=== 'a') || (newGuess=== 'l') || (newGuess=== 'i')){
+    alert('Correct answer! \'' + newGuess + '\' is part of \'ali\'');
+    corAnswer++;
+    break;
+  } else{
+    newAttempts-=1;
+    alert('Wrong letter... you have ' + newAttempts + ' attempts left!');
+    if(!newAttempts){
+      alert('No more attempts.. correct answer is ' + ali + '!');
+      break;
+    }
+  }
+}
+
+alert('Well... your score is '+ corAnswer + ' out of 7!!!!');
